ed683a88dc1fc39bc65e41bd2b1411d8ef58f943
Observon <erickoliveirarangel@gmail.com>
2026-04-03 23:37:58 -0300

    fix: relax helmet csp for swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,7 +9,11 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    }),
+  );
 
   const authRateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
